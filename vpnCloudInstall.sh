f5b670124fe303a640a46c6d6416218e7b23a81d
euivmar <euivmar@gmail.com>
2023-05-18 15:14:09 +0200

    Add files via upload
    Add vpncloud install

---
+++ vpnCloudInstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# source: https://vpncloud.ddswd.de/install/
+echo "deb https://repo.ddswd.de/deb stable main" | sudo tee /etc/apt/sources.list.d/vpncloud.list
+wget https://repo.ddswd.de/deb/public.key -qO - | sudo apt-key add
+sudo apt update
+sudo apt install vpncloud
